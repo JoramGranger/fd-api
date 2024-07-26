aa57d01557d772f93338b830b9b3f3c50dc239cd
joram <joramgranger@outlook.com>
2024-07-25 17:19:30 -0700

    product-sort

--- controllers/productController.js
+++ controllers/productController.js
@@ -65,10 +65,51 @@ exports.deleteProduct =  async (req, res) => {
 // search
 exports.searchProducts = async (req, res) => {
     try {
-        const query = req.query.query;
-        const products = await Product.find({ name: new RegExp(query, 'i') });
+      const { q } = req.query;
+      console.log('Search query:', q);
+      if (!q) {
+        return res.status(400).json({ msg: 'Query parameter is required' });
+      }
+  
+      const regex = new RegExp(q, 'i'); 
+      console.log('Regex for search:', regex);
+      const products = await Product.find({
+        $or: [
+          { name: regex },
+          { description: regex },
+          { category: regex }
+        ]
+      });  
+      res.status(200).json(products);
+    } catch (err) {
+      console.error('Error occurred during product search:', err); 
+      res.status(500).json({ msg: 'Server error' });
+    }
+  };
+
+// category sort
+exports.getProductsByCategory = async(req, res) => {
+    try {
+        const category = req.params.category;
+        const products = await Product.find({ category });
         res.status(200).json(products);
-    } catch(err) {
-        res.status(500).json({msg: 'Server error'});
+    } catch (err) {
+        res.status(500).json({msg: 'Server Error'});
+    }
+};
+
+// stock
+exports.updateProductStock = async (req, res) => {
+    try {
+        const {stock} = req.params.body;
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            { stock },
+            { new: true, runValidators: true }
+        );
+        if(!product) return res.status(404).json({msg: 'Product not found'});
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json({msg: 'Server error '});
     }
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,10 +6,13 @@ const auth = require('../middleware/auth');
 // pr
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
+router.get('/search/', productController.searchProducts)
+router.get('/category/:category', productController.getProductsByCategory);
 
 // ptdr
 router.post('/', auth.protect, auth.admin, productController.createProduct);
 router.put('/:id', auth.protect, auth.admin, productController.updateProduct);
+router.patch('/:id/stock', auth.protect, auth.admin, productController.updateProductStock);
 router.delete('/:id', auth.protect, auth.admin, productController.deleteProduct);
 
 module.exports =  router;
