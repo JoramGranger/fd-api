b0d3acdb6fbe1ef4b1286b3d444e2a2b4ced18a0
joram <joramgranger@outlook.com>
2024-07-31 12:51:16 +0300

    segregative upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,21 @@
+const { fstat } = require('fs');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
 
 // Set up storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    let uploadPath = 'uploads/';
+    if (req.baseUrl.includes('product')) {
+      uploadPath += 'products/';
+    } else if (req.baseUrl.includes('user')) {
+      uploadPath += 'users/';
+    }
+
+    fs.mkdirSync(uploadPath, { recursive: true });
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
